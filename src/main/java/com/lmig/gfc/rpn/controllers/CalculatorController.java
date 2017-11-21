5a57e62c488f7193a841540809c8d42c4cadc55b
David Hall <david.hall@libertymutual.com>
2017-11-21 15:57:39 -0500

    refactored code to add interfaces, inheritance, abstraction.  also added additional operations

--- src/main/java/com/lmig/gfc/rpn/controllers/CalculatorController.java
+++ src/main/java/com/lmig/gfc/rpn/controllers/CalculatorController.java
@@ -7,16 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lmig.gfc.rpn.models.AbsCalculator;
+import com.lmig.gfc.rpn.models.Adder;
+import com.lmig.gfc.rpn.models.Divider;
+import com.lmig.gfc.rpn.models.Multiplier;
 import com.lmig.gfc.rpn.models.OneArgumentUndoer;
+import com.lmig.gfc.rpn.models.PushUndoer;
+import com.lmig.gfc.rpn.models.Subtracter;
 import com.lmig.gfc.rpn.models.TwoArgumentUndoer;
+import com.lmig.gfc.rpn.models.Undoer;
 
 @Controller
 public class CalculatorController {
 	private Stack<Double> stack;
-	private OneArgumentUndoer undoer;
+	private Stack<Undoer> undoers;
+	private AbsCalculator calc;
 	
 	public CalculatorController() {
 		stack = new Stack<Double>();
+		undoers = new Stack<Undoer>();
 	}
 	
 	@GetMapping("/")
@@ -25,14 +34,14 @@ public class CalculatorController {
 		mv.setViewName("default");
 		mv.addObject("stack",stack);
 		mv.addObject("hasTwoOrMoreNumbers", stack.size()>= 2);
-		mv.addObject("hasUndoer", undoer != null);
+		mv.addObject("hasUndoer", undoers.size()>0);
 		return mv;
 	}
 	
 	@PostMapping("/enter")
 	public ModelAndView pushNumberOntoStack(double value) {
 		stack.push(value);
-		undoer = null;
+		undoers.push(new PushUndoer());
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 		return mv; 
@@ -40,11 +49,10 @@ public class CalculatorController {
 	
 	@PostMapping("/add")
 	public ModelAndView addTwoNumbers() {
-		double first = stack.pop();
-		double second = stack.pop();
-		double result = first + second;
-		stack.push(result);
-		undoer = new TwoArgumentUndoer(first, second);
+		Adder adder = new Adder(stack);
+		adder.goDoIt();
+		undoers.push(adder);
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 		return mv;  
@@ -52,11 +60,27 @@ public class CalculatorController {
 	
 	@PostMapping("/minus")
 	public ModelAndView subtractTwoNumbers() {
-		double first = stack.pop();
-		double second = stack.pop();
-		double result = second-first;
-		stack.push(result);
-		undoer = new TwoArgumentUndoer(first, second);
+		Subtracter subtracter = new Subtracter(stack);
+		subtracter.goDoIt();
+		undoers.push(subtracter);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
+		return mv; 
+	} 
+	@PostMapping("/divide")
+	public ModelAndView divideTwoNumbers() {
+		Divider divider = new Divider(stack);
+		divider.goDoIt();
+		undoers.push(divider);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
+		return mv; 
+	} 
+	@PostMapping("/multiply")
+	public ModelAndView multiplyTwoNumbers() {
+		Multiplier multiplier = new Multiplier(stack);
+		multiplier.goDoIt();
+		undoers.push(multiplier);
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 		return mv; 
@@ -64,8 +88,8 @@ public class CalculatorController {
 	
 	@PostMapping("/undo")
 	public ModelAndView undo() {
+		Undoer undoer = undoers.pop();
 		undoer.undo(stack);
-		undoer = null;
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 		return mv; 
@@ -73,10 +97,12 @@ public class CalculatorController {
 	
 	@PostMapping("/abs")
 	public ModelAndView abs() {
-		double number = stack.pop();
-		double abs = Math.abs(number);
-		stack.push(abs);
-		undoer = new OneArgumentUndoer(number);
+//		double number = stack.pop();
+//		double abs = Math.abs(number);
+//		stack.push(abs);
+		calc = new AbsCalculator();
+		double number = calc.calculateAbs(stack);
+		undoers.push(new OneArgumentUndoer(number));
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 		return mv; 
---
+++ src/main/java/com/lmig/gfc/rpn/models/TwoNumberCalculation.java
@@ -0,0 +1,32 @@
+package com.lmig.gfc.rpn.models;
+
+import java.util.Stack;
+
+public abstract class TwoNumberCalculation implements Undoer{
+
+	private Stack<Double> stack;
+	private Undoer undoer;
+
+	public TwoNumberCalculation(Stack<Double> stack) {
+		this.stack = stack;
+	}
+
+	public void goDoIt() {
+		double first = stack.pop();
+		double second = stack.pop();
+		double result = doMath(first, second);
+		stack.push(result);
+		undoer = new TwoArgumentUndoer(first, second);
+	}
+	
+	protected abstract double doMath(double firstNumber, double secondNumber);
+	
+	@Override
+	public void undo(Stack<Double> stack) {
+		// TODO Auto-generated method stub
+		undoer.undo(stack);
+		
+	}
+	
+
+}
